44357ec8493e1b64e2aa9d996abb5cd84eae32ae
xianyu <yanxianyu@didiglobal.com>
2021-04-21 20:25:05 +0800

    feat: up

---
+++ up.sh
@@ -0,0 +1,29 @@
+version=$1
+
+versions=("0.9","0.9.3","0.10","0.10.0","0.11","0.11.0","0.12","0.12.0","latest")
+
+if [ -z "$version" ]; then 
+    version="latest"
+fi
+
+if [[ "${versions[@]}"  =~ "$version" ]]; then
+    echo "Install thrift version: $version"
+else
+    echo "Thrift version not available: version=$version: available version $versions"
+    exit 1
+fi
+
+echo "Pull docker image: thrift:$version"
+docker pull "thrift:$version"
+
+echo "Write proxy command file to /usr/local/thrift"
+
+proxy_file="docker run --rm -v \"\$PWD:\$PWD\" -w \$PWD -u \`id -u\` thrift:$version thrift \"\$@\""
+
+sudo echo "$proxy_file" > "/usr/local/bin/thrift"
+
+sudo chmod +x "/usr/local/bin/thrift"
+
+echo "Done"
+
+thrift --version
